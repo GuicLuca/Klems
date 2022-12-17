b5d5a0df64b794b83f98b94d16d9a66cc53a03f7
LORI LOU <llou@etu.uqac.ca>
2022-12-17 00:04:07 -0500

    added first version of infection file

---
+++ Klems/Source/Klems/Private/Infection/AKSInfectionTile.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Klems/Public/Infection/AKSInfectionTile.h"
+
+
+// Sets default values
+AAKSInfectionTile::AAKSInfectionTile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAKSInfectionTile::Infect_Implementation(float InfectAmount)
+{
+	if(!TileDynamicMat) return;
+
+	float InfectionDensity;
+	TileDynamicMat->GetScalarParameterValue(FHashedMaterialParameterInfo("InfectionDensity"),InfectionDensity);
+
+	//if(!ensureMsgf(InfectionDensity, "Material Instance do not have any scalar parameter %s")) return;
+	
+	TileDynamicMat->
+}
+
+// Called when the game starts or when spawned
+void AAKSInfectionTile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UMaterialInterface* objectMaterial = this->Tile->GetMaterial(0);
+
+	this->TileDynamicMat = UMaterialInstanceDynamic::Create(objectMaterial, Tile);
+}
+
+

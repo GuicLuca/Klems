c062a46ef2c46d551da9d7e598b9e1b1a5539faa
LORI LOU <llou@etu.uqac.ca>
2022-12-16 22:54:31 -0500

    initial commit

---
+++ Klems/Source/Klems/KlemsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KlemsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KLEMS_API AKlemsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

fcbab990531928f9fc638fc44c5786588bdafc2a
HUGO MORA <hmora@etu.uqac.ca>
2022-12-17 11:57:37 -0500

    Base weapon

---
+++ Klems/Source/Klems/Private/Weapons/KSWeapon.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapons/KSWeapon.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+AKSWeapon::AKSWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Weapon Mesh"));
+
+}
+
+void AKSWeapon::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AKSWeapon,Ammo);
+}
+
+void AKSWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AKSWeapon::Reload_Implementation()
+{
+}
+
+void AKSWeapon::Fire_Implementation()
+{
+}
+
+
+

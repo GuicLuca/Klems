5f95263e9228f2175ba9973262d4e8e2cf8de549
Lucas Guichard <lucasguichard127@gmail.com>
2022-12-18 16:43:41 -0500

    Feat - update the loby gamemode

---
+++ Klems/Source/Klems/Public/GameMode/KSLobbyGameMode.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "UObject/Object.h"
+#include "KSLobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KLEMS_API AKSLobbyGameMode : public AGameMode
+{
+public:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+private:
+	GENERATED_BODY()
+};
